d689e066b517506583111e89870bd580c929125e
Patrick Dolan <dolanp1992@gmail.com>
2022-12-08 21:37:03 -0800

    refactor: create openNav and closeNav functions to dry up code

--- main.js
+++ main.js
@@ -1,43 +1,41 @@
+const openNav = () => {
+  // Show shader
+  $("#shader").fadeIn(500)
+    
+  // Show mobile nav
+  $("#mobile-nav").removeClass("mobile-nav__inactive")
+  $("#mobile-nav").addClass("mobile-nav__active")
+  $("#mobile-nav").animate({
+    width: "70%"
+  }, 500)
+}
+
+const closeNav = () => {
+  // Hide shader
+  $("#shader").fadeOut(500);
+    
+  // Hide mobile nav
+  $("#mobile-nav").animate({
+    width: "0%"
+  }, 500, () => {
+    $("#mobile-nav").removeClass("mobile-nav__active")
+    $("#mobile-nav").addClass("mobile-nav__inactive")
+  })
+}
+
 $(() => {
   // Open mobile nav
   $(".icon-menu").click(() => {
-    // Show shader
-    $("#shader").fadeIn(500)
-    
-    // Show mobile nav
-    $("#mobile-nav").removeClass("mobile-nav__inactive")
-    $("#mobile-nav").addClass("mobile-nav__active")
-    $("#mobile-nav").animate({
-      width: "70%"
-    }, 500)
+    openNav();
   });
-
 
   // Close mobile nav with close img
   $(".icon-menu-close").click(() => {
-    // Hide shader
-    $("#shader").fadeOut(500);
-    
-    // Hide mobile nav
-    $("#mobile-nav").animate({
-      width: "0%"
-    }, 500, () => {
-      $("#mobile-nav").removeClass("mobile-nav__active")
-      $("#mobile-nav").addClass("mobile-nav__inactive")
-    })
+    closeNav();
   });
 
   // Close mobile nav if shader is clicked 
   $("#shader").click(() => {
-    // Hide shader
-    $("#shader").fadeOut(500);
-    
-    // Hide mobile nav
-    $("#mobile-nav").animate({
-      width: "0%"
-    }, 500, () => {
-      $("#mobile-nav").removeClass("mobile-nav__active")
-      $("#mobile-nav").addClass("mobile-nav__inactive")
-    })
+    closeNav();
   })
 })
