e9e2a352d156361d9e57d64ff70cc570385cd4e5
Patrick Dolan <dolanp1992@gmail.com>
2022-12-08 21:11:05 -0800

    update: add jquery to hide and show mobile nav    update: add mobile nav to nav section of html     styling: add styling to mobile new movile nav

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 $(() => {
-  
+  // Open mobile nav
+  $(".icon-menu").click(() => {
+    // Show shader
+    $("#shader").fadeIn(500)
+    
+    // Show mobile nav
+    $("#mobile-nav").removeClass("mobile-nav__inactive")
+    $("#mobile-nav").addClass("mobile-nav__active")
+    $("#mobile-nav").animate({
+      width: "70%"
+    }, 500)
+  });
+
+
+  // Close mobile nav with close img
+  $(".icon-menu-close").click(() => {
+    // Hide shader
+    $("#shader").fadeOut(500);
+    
+    // Hide mobile nav
+    $("#mobile-nav").animate({
+      width: "0%"
+    }, 500, () => {
+      $("#mobile-nav").removeClass("mobile-nav__active")
+      $("#mobile-nav").addClass("mobile-nav__inactive")
+    })
+  });
 })
